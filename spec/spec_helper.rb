78a0c05e7b3e8f0d72371ba6d3e82796aa5cc81a
TobiasAndre <tobiasae@gmail.com>
2018-12-10 21:38:22 -0200

    adding tests and controller

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,3 +94,7 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
+require File.expand_path("../../config/environment", __FILE__)
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
